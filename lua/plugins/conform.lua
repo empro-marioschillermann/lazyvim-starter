ebc253e2b853aafcbe7e45fbbb70461b786fef4b
Mario Schillermann <mario.schillermann@empro.de>
2024-12-06 08:19:26 +0100

    set prettier as formatter

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,11 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      html = { { "prettierd" } },
+      javascript = { { "prettierd" } },
+      markdown = { { "prettierd" } },
+      typescript = { { "prettierd" } },
+    },
+  },
+}
